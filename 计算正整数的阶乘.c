b74712a0e3971db401307f7fc67a97765d016a5b
ymxgoalwin <yedongxin070525@qq.com>
2025-11-04 22:25:57 +0800

    Add files via upload

---
+++ 计算正整数的阶乘.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int main(void) {
+
+     //让用户输入一个正整数
+    printf("请输入一个正整数：");
+
+    //声明一个变量uNumber，此变量用于存放用户输入的数字
+    uint64_t uNumber;
+
+    //将用户输入的数字存放在uNumber中
+    scanf_s("%"PRIu64"", &uNumber);
+
+
+    //初始化变量times,用来进行循环判断
+    uint64_t times = 1;
+
+    //初始化一个变量uResult，用来存放用户输入的正整数的阶乘
+    uint64_t uResult = 1;
+
+    //进行while循环，当times不大于用户输入的数时运行
+    while (times <= uNumber) {
+
+        //将1与自然数进行相乘，再将结果赋值给变量uResult
+        uResult *= times;
+
+        //将变量加1后再赋值给变量times
+        times += 1;
+    }
+
+    //输出用户输入的正整数的阶乘
+    printf("您输入的数字的阶乘为：%"PRIu64"",uResult);
+
+
+
+   
+
+    return 0;
+
+}
